cfda50a8920a5052c1a7c1e2b015488ce707320d
Jakub Trzeciak <jp3@jptrzy.xyz>
2025-03-21 21:16:29 +0100

    simple print

--- src/print.cpp
+++ src/print.cpp
@@ -1,5 +1,20 @@
 #include "node.h"
 #include <iostream>
 
+void _simple_print(Node* node, int depth) {
+
+  for (int i = 0; i < depth; i++) std::cout << "  ";
+  std::cout << node->name << "\n";
+
+  if (node->first_child) {
+    _simple_print(node->first_child, depth+1);
+  }
+
+  if (node->sibling) {
+    _simple_print(node->sibling, depth);
+  }
+}
+
 void simple_print(Node* root) {
+  _simple_print(root, 0);
 }
